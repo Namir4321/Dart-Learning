2e688180c69e8e97553fea4e1e2816cfab20c522
Namir <nk.namirkhan1@gmail.com>
2026-01-11 20:44:53 +0530

    cascade peratr

---
+++ class-advanced/Credentials.dart
@@ -0,0 +1,22 @@
+class Credentials {
+  const Credentials({this.email="",this.password=""});
+  final String email;
+  final String password;
+
+  Credentials copyWith({
+    String? email,
+    String? password,
+  }){
+    return Credentials(
+      email:email ?? this.email,
+      password:password ?? this.password,
+    );
+  }
+  @override
+  String toString()=>'Credentials($email , $password)';
+}
+void main(){
+  const credentials=Credentials();
+  final updated1=credentials.copyWith(email: "example@example.com");
+  print(updated1);
+}
---
+++ class-advanced/path.dart
@@ -0,0 +1,21 @@
+import 'equtable-operator.dart';
+
+class ClosedPath {
+List<Point> _points=[];
+
+void moveTo(Point point){
+  _points=[point];
+}
+void lineTo(Point point){
+  _points.add(point);
+}
+}
+
+void main(){
+  final path=ClosedPath()
+  ..moveTo(Point(0, 0))
+  ..moveTo(Point(0, 0))
+  ..moveTo(Point(0, 0))
+  ..moveTo(Point(0, 0))
+  ..moveTo(Point(0, 0));
+}
