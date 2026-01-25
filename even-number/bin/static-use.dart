3d9ab6c59d80ee5b10bf9ff566207175bb15e1fc
Namir <nk.namirkhan1@gmail.com>
2026-01-25 21:44:04 +0530

    learnig the static wrd use

---
+++ even-number/bin/static-use.dart
@@ -0,0 +1,12 @@
+import 'static.dart';
+
+class Test {
+  test() {
+    Addition.add(20,30);
+  }
+}
+
+void main() {
+  Test obj = Test();
+  obj.test();
+}
---
+++ even-number/bin/static.dart
@@ -0,0 +1,15 @@
+class Addition {
+  static add(int num1, int num2) {
+    int sum = num1 + num2;
+    print(sum);
+  }
+}
+// we can use the static wrd where we want t use the same peratin multiple time
+// rather than writting the repatitative cde fr eg 
+// if we want t calculate the price f any car in 
+// car purchasing app and shw the user the n rad price 
+// fr a car that has included all the tax and insurance 
+// we can create a static methd r class where we can 
+// write the lgic t calculat the price and use it in multiple place
+// just like the exprt mdule in js r cmpnent in react and nextjs
+
